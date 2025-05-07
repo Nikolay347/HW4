12b9e18611761ea3c12614f54dc2e95db464b20e
Nikolay347 <nickpkinn@gmail.com>
2025-05-07 21:58:58 +0300

    Added field rating to student for migration check. Migration crearing. Migration creating to db.

--- Dj_project/first_app/models/student.py
+++ Dj_project/first_app/models/student.py
@@ -13,6 +13,7 @@ class Student(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     course = models.ForeignKey('Course', on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=20, blank=True)
+    rating = models.CharField(max_length=20, blank=True)
 
     def __str__(self):
         return self.name
